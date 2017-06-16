9d385d9fa8e5cf87a951f5a1bd0fce9cb74262ba
MSe <mse1969@posteo.de>
2017-06-16 12:19:09 +0200

    Remove Google Play Services dependency
    Backport of https://review.lineageos.org/#/c/162231/
    
    Change-Id: I0b9e62df4caa4dd60e715e01fe68bb87560ccf93

--- src/com/cyanogenmod/lockclock/weather/WeatherUpdateService.java
+++ src/com/cyanogenmod/lockclock/weather/WeatherUpdateService.java
@@ -39,8 +39,6 @@ import com.cyanogenmod.lockclock.ClockWidgetProvider;
 import com.cyanogenmod.lockclock.misc.Constants;
 import com.cyanogenmod.lockclock.misc.Preferences;
 import com.cyanogenmod.lockclock.misc.WidgetUtils;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
 
 import java.util.Date;
 
@@ -188,7 +186,7 @@ public class WeatherUpdateService extends Service {
                 String locationProvider = lm.getBestProvider(sLocationCriteria, true);
                 if (TextUtils.isEmpty(locationProvider)) {
                     Log.e(TAG, "No available location providers matching criteria.");
-                } else if (isGooglePlayServicesAvailable()
+                } else if (PackageManagerUtils.isAppInstalled(mContext, "com.google.android.gms")
                         && locationProvider.equals(LocationManager.GPS_PROVIDER)) {
                     // Since Google Play services is available,
                     // let's conserve battery power and not depend on the device's GPS.
@@ -199,12 +197,6 @@ public class WeatherUpdateService extends Service {
             }
 
             return location;
-        }
-
-        private boolean isGooglePlayServicesAvailable() {
-            int result = GooglePlayServicesUtil.isGooglePlayServicesAvailable(mContext);
-            return result == ConnectionResult.SUCCESS
-                    || result == ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED;
         }
 
         @Override
